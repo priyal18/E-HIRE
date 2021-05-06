58d8e7481e5fce106a5a3781124c8e62f7038e05
priyal18 <priyal930gupta@gmail.com>
2021-05-06 12:13:32 +0530

    cors

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
-const io = require('socket.io')(server);
+const io = require("socket.io")(server, {
+    cors: {
+      origin: '*'
+    }
+});
 const {v4:uuidv4 } = require('uuid');
 const { ExpressPeerServer } = require('peer');
 const peerServer = ExpressPeerServer(server, {
